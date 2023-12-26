dc9d37c3b6b4b0caeaa2b2b252142e5035fffc64
Maschinenmann <herceclase@gmail.com>
2023-12-26 19:15:54 +0100

    reverted

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+/////////////////////////////////////
+/// GLOBAL VARIABLES
+/////////////////////////////////////
+
 const TIME_DELAY = 3000
 
+/////////////////////////////////////
+/// ONLOAD EVENTS
+/////////////////////////////////////
+
 function onLoad() {
-    setAudioVolume()
     setTimeout(function () {
         activateScroll()
         playMainVideo()
         stopLoadingVideo()
     }, TIME_DELAY);
-}
-
-function setAudioVolume() {
-    var mainAudio = document.getElementById("main-audio");
-    mainAudio.volume = 0.3;
 }
 
 function activateScroll() {
@@ -20,12 +22,63 @@ function activateScroll() {
 }
 
 function playMainVideo() {
-    var mainVideo = document.getElementById("main-video");
-    mainVideo.style.visibility = "visible";
-    mainVideo.play();
+    var mainVideo = document.getElementsByClassName("main-video");
+    for (let index = 0; index < mainVideo.length; index++) {
+        mainVideo[index].style.visibility = "visible";
+        mainVideo[index].play();
+    }
 }
 
 function stopLoadingVideo() {
     var loadingVideo = document.getElementById("loading-video");
     loadingVideo.style.visibility = "hidden";
 }
+
+/////////////////////////////////////
+/// SCROLL EVENTS
+/////////////////////////////////////
+
+$("#main-title").on("scroll", function () {
+    console.log(window.scrollY)
+    if (window.scrollY > 870) {
+        $(this).removeClass("hidden");
+    }
+});
+
+$("#main-title").on("load", function () {
+    if (window.scrollY > 870) {
+        $(this).removeClass("hidden");
+    }
+});
+
+$.fn.moveIt = function () {
+    var $window = $(window);
+    var instances = [];
+    $(this).each(function () {
+        instances.push(new moveItItem($(this)));
+
+    });
+
+    window.onscroll = function () {
+        if (window.scrollY > 870 && window.scrollY < 3000) {
+            var scrollTop = $window.scrollTop();
+            instances.forEach(function (inst) {
+                inst.update(scrollTop);
+            });
+        }
+    }
+}
+
+var moveItItem = function (el) {
+    this.el = $(el);
+    this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function (scrollTop) {
+    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+// Initialization
+$(function () {
+    $('[data-scroll-speed]').moveIt();
+});
